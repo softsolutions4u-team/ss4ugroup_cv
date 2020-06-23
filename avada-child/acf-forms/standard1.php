c45d98a7df9556801c31648de9731a94bfd2096b
gunapathi <guna@softsolutions4u.info>
2020-06-23 16:03:57 +0530

    Vinoth Resolved console error on form

--- avada-child/acf-forms/standard1.php
+++ avada-child/acf-forms/standard1.php
@@ -923,45 +923,7 @@ $(document).ready(function () {
 		<input type="hidden" name="action" value="resume" />
 	</div>
 </form>
-<!-- The Modal -->
-<div id="myModal" class="modal">
-
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close">&times;</span>
-    <div id="home" class="banner page-section-1">
-        <?php get_template_part( 'templates/template', 'theme-3' ); ?>
-    </div>
-  </div>
-
-</div>
-
 <script>
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 
 var _URL = window.URL || window.webkitURL;
 $(".form-file-upload").change(function(e) {
