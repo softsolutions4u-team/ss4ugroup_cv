5b4f654e1d90c950718a76d0b923f8bbf251eeba
gunapathi <guna@softsolutions4u.info>
2020-06-24 19:09:40 +0530

    Allwin added Previe, Edit, Publish and Delete

--- avada-child/woocommerce/myaccount/my-account-resume.php
+++ avada-child/woocommerce/myaccount/my-account-resume.php
@@ -17,68 +17,74 @@
  * @package     WooCommerce/Templates
  * @version     2.6.0
  */
-
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly
 }
 ?>
 <h2> Resume </h2>
 <div>
 	<?php
-		$default_template = 'standard1';
-		// esc_url( get_permalink( get_option('woocommerce_myaccount_page_id') ) .'?template=standard1' );
-		// $customer_id = get_current_user_id();
+	$default_template = 'standard1';
+	// esc_url( get_permalink( get_option('woocommerce_myaccount_page_id') ) .'?template=standard1' );
+	// $customer_id = get_current_user_id();
 	?>
-	<div id="content">
+    <div id="content">
 		<?php
+		$template_configs = [
+			'standard1' => 'page-template-standard1.php',
+			'standard2' => '', // TODO
+			'standard3' => '', // TODO
+			'standard4' => '', // TODO
+		];
 
-			$template_configs = [
-				'standard1' => 'page-template-standard1.php',
-				'standard2' => '', // TODO
-				'standard3' => '', // TODO
-				'standard4' => '', // TODO
-			];
+		if (isset($_GET['id'])) {
+			$edit_post_id = $_GET['id'];
+			$page_template = get_page_template_slug($edit_post_id);
+			$template_name = array_search($page_template, $template_configs);
+			$_GET['template'] = $template_name;
+		}
 
-			if (isset($_GET['id'])) {
-				$edit_post_id = $_GET['id'];
-				$page_template = get_page_template_slug( $edit_post_id );
-				$template_name = array_search ($page_template, $template_configs);
-				$_GET['template'] = $template_name;
-			}
-			
-			$input_template = isset($_GET['template']) ? $_GET['template'] : $default_template;
+		$input_template = isset($_GET['template']) ? $_GET['template'] : $default_template;
 
-			
-			$resume_filter_args = array(
-				'author'        => get_current_user_id(), // I could also use $user_ID, right?
-				'post_type'     => 'resume',
-				'orderby'       => 'post_date',
-				'order'         => 'ASC' 
-			);
 
-			// get his posts 'ASC'
-			$logged_user_resumes = get_posts( $resume_filter_args );
+		$resume_filter_args = array(
+			'author' => get_current_user_id(), // I could also use $user_ID, right?
+			'post_type' => 'resume',
+			'orderby' => 'post_date',
+			'post_status' => 'any',
+			'order' => 'ASC'
+		);
 
-			$resume_post_id = 0; ?>
-		<div class="row">
-		<?php
+		// get his posts 'ASC'
+		$logged_user_resumes = get_posts($resume_filter_args);
+
+		$resume_post_id = 0;
+		?>
+        <div class="row">
+			<?php
 			foreach ($logged_user_resumes as $user_resume) {
 				?>
-		
-		<div class="card col-sm-3 p-0 mr-2 mb-2" style="width: 8rem;">
-			  <div class="card-body">
-				  <?php 
-							$edit_url = get_permalink().'standard'. '?id=' . $user_resume->ID;
+
+				<div class="card col-sm-3 p-0 mr-2 mb-2" style="width: 8rem;">
+					<div class="card-body">
+						<?php
+						$edit_url = get_permalink() . 'standard' . '?id=' . $user_resume->ID;
 						?>
-					<h5 class="card-title text-center"><?php echo get_the_title( $user_resume->ID ); ?></h5>
-				  <div class="text-center">
-					  <a href="<?php echo get_post_permalink($user_resume->ID ); ?>" target="_blank" class="card-link">View</a>
-					  <a href="<?php echo esc_url( $edit_url)?>" class="card-link">Edit</a>
-				  </div>
-			  </div>
-			</div>
-		
-		<?php
+						<h5 class="card-title text-center"><?php echo get_the_title($user_resume->ID); ?></h5>
+						<div class="text-center">
+							<a href="<?php echo (get_post_status($user_resume->ID) == 'draft') ? (get_home_url() . '/?post_type=resume&p=' . $user_resume->ID . '&preview=true') : ( get_post_permalink($user_resume->ID)) ?>" target="_blank" class="card-link">                                
+								Preview
+							</a>
+							<a href="<?php echo esc_url($edit_url) ?>" class="card-link">Edit</a>
+							<a onclick="return confirm('Are you Sure you want to Delete this post?')" href="<?php echo get_delete_post_link($user_resume->ID) ?>" class="card-link">Delete</a>
+							<a href="<?php //  echo wp_publish_post($user_resume->ID)  ?>" class="card-link">
+								Publish
+							</a>
+						</div>
+					</div>
+				</div>
+
+				<?php
 				$resume_post_id = $edit_post_id;
 // 				$page_template = get_page_template_slug( $user_resume->ID );
 // 				$template_name = array_search ($page_template, $template_configs);
@@ -88,7 +94,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 // 				}
 			}
 			//require_once( get_stylesheet_directory() . "/acf-forms/" . $input_template . ".php" );
-		?>
-			</div>
-	</div>
+			?>
+        </div>
+    </div>
 </div>
